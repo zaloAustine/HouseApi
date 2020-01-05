b649c11eae6bd9f1d0edfede146da73b8cd0ae88
ZaloAustine <austinezaloa@gmail.com>
2020-01-05 11:53:42 -0500

    runtime-update api

--- HouseApi/HouseApiMain/settings.py
+++ HouseApi/HouseApiMain/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'o)p!r_gy1y_b0%!vx^h(bl5u+nm(pc=tu#)&ahiyn^jgig%ker'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['housenew.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +130,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
