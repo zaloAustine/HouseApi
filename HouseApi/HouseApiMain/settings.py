5c281e21fd4c938bf3f5d4c230813cdecdfe2347
ZaloAustine <austinezaloa@gmail.com>
2020-01-05 00:21:47 -0500

    runtime-update api

--- HouseApi/HouseApiMain/settings.py
+++ HouseApi/HouseApiMain/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'),)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
